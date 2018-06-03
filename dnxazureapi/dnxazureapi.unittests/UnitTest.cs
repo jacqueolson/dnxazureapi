6ad51dddf3314b97392392d786cd61d9757ad9b9
Jacque Olson <jacque.w.olson@gmail.com>
2018-06-02 22:12:40 -0500

    Initial

---
+++ dnxazureapi/dnxazureapi.unittests/UnitTest.cs
@@ -0,0 +1,33 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace dnxazureapi.unittests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //dummy test, not doing anything obviously
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //dummy test, not doing anything obviously
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            //dummy test, not doing anything obviously
+            Assert.AreEqual(1, 1);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            //dummy test, not doing anything obviously
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ dnxazureapi/dnxazureapi/Controllers/FruitController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dnxazureapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FruitController : ControllerBase
+    {
+        // GET api/fruit
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "fruit1", "fruit2" };
+        }
+
+        // GET api/fruit/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "fruit#" + id;
+        }
+
+        // POST api/fruit
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] string value)
+        {
+            return $"{value} has been created";
+        }
+
+        // PUT api/fruit/5
+        [HttpPut("{id}")]
+        public ActionResult<string> Put(int id, [FromBody] string value)
+        {
+            return $"{id} with {value} has been updated";
+        }
+
+        // DELETE api/fruit/5
+        [HttpDelete("{id}")]
+        public ActionResult<string> Delete(int id)
+        {
+            return $"{id} has been deleted";
+        }
+    }
+}
